b776c6807024ff13ed4cb12e14b461fa01cb6ceb
GSTran <giovanniseverinotran@gmail.com>
2025-03-30 18:38:48 -0700

    Add base files

---
+++ insight-api.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from deepface import DeepFace
+
+app = FastAPI()
+
+@app.get('/')
+def hello_world():
+    result = DeepFace.verify(img1_path = "img1.jpg", img2_path = "img2.jpg")
+    return {'Result' : result.verified}
+
